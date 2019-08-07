0a9da3035b4fb762b7c35e386acff71924a51958
15knots <15knots@users.noreply.github.com>
2019-08-07 22:17:35 +0200

    adjust to newer CDT

--- C-inclpaths/src/main.c
+++ C-inclpaths/src/main.c
@@ -3,12 +3,20 @@
 
 // Clicking 'Open Declaration' should open config.h
 #include "config.h"
+// Clicking 'Open Declaration' should open include/included.h
+#include <included.h>
 
 int main(void) {
-#if MACRO_FROM_C_INCLPATH_PRJ-0
+#if MACRO_FROM_CONFIGURE_FILE-0
 	puts("!!!Hello World!!!" );
 #endif
-#if MACRO_FROM_COMMANDLINE-0
+#if MACRO_FROM_ADD_DEFINITIONS-0
+	puts("!!!Hello, hello World!!!" );
+#endif
+#if MACRO_FROM_INCLUDE_DIRECTORIES_BELOW-0
+	puts("!!!Hello, hello World!!!" );
+#endif
+#if MACRO_FROM_INCLUDE_DIRECTORIES_ABOVE-0
 	puts("!!!Hello, hello World!!!" );
 #endif
 #if __GNUC__-0
